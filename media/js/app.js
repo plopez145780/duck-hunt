76c6f9def2bbe6dd526867c113cd0a52af986c8f
plopez <pierre_albert_lopez@hotmail.fr>
2016-09-09 00:58:00 +0200

    Lancement via boutton
    Ajout d'un menu avec boutton et du score
    Refactorisation de fonction
    Ajustement des hit box
    Ajout du déplacement de droite a gauche

--- media/js/app.js
+++ media/js/app.js
@@ -1,83 +1,125 @@
-/* Variables Globales */
-// référence a l'objet duck
-var duck = $("#duck");
-// Nombre de canard restant lors de la partie
-var nbDuck = 10;
-// Largeur de la fenètre
-var largeurFenetre = $(window).width();
-// Hauteur de la fenètre
-var hauteurFenetre = $(window).height();
+/* Varialbe Globales */
+duck = $("#duck");// référence a l'objet duck
 
-function relanceCanard() {
-	if(nbDuck != 0){
-		console.log(nbDuck);
-		deplacerCanard();
-	}
-	else {
-		duck.stop(true, false);
+/* Détermine si tous les canard (tour) on été joué */
+function checkFinJeu() {
+	if(nbDuckRestant != 0){
+		tourDeJeu();
+	} else {
+		//TODO Changer par un texte dans la fenètre
+		alert("Game Over ! Score de " + playerName + " : " + score);
 	}
 }
-/*  */
+
+/* tue le canard */
 function tuerCanard() {
-	console.log('pan !');
-	// Arrète le déplacement horizontal
-	duck.stop(true, false);
+	score++;// Incrémente le score
+	$("#score").html("Score : " + score);// Affiche le nouveau score
+	duck.stop(true, false);// Arrète le déplacement horizontal du canard
 	
-	// Change l'image
+	// Change les propriété de l'image
 	duck.css({
+		'width': '62px',
+		'height': '90px',
 		'background-image': 'url("media/img/duck-dead.gif")',
+		'background-position': '-50px -20px',
 	});
 
 	// Animation
 	duck.animate({
 		top: hauteurFenetre,},
 		1000, function() {
-			console.log('mort');
-			relanceCanard();
+			checkFinJeu();
 	});
 
 }
 
-/* Déplacement du canard de gauche a droite */
-function deplacerCanard(){
-	// Décrémente le nombre de canard restant
-	nbDuck--;
-
-	// Point de départ a hauteur aléatoire
-	var hauteurRand = Math.random() * (hauteurFenetre - duck.height());
-	
+/* Animation de déplacement de gauche a droite */
+function deplacerGaucheDroite(){
 	//Initialise les propriétés de départ de l'image du canard
 	duck.css({
-		'left': '-50px',
-		'top': hauteurRand,
+		'width': '85px',
+		'height': '70px',
 		'background-image': 'url("media/img/duck-right.gif")',
-		'width': '130px',
-		'height': '104px',
+		'background-position': '-30px -10px',
+		'left': '-' + duck.width() + 'px',
+		'top': hauteurRandDepart,
+		
 	});
-
-	console.log(hauteurRand);
 
 	// Déplace le canard
 	duck.animate({
-		left: largeurFenetre,},
-		5000, function() {
-			console.log('passé');
-			relanceCanard();
+		left: largeurFenetre,
+		top: hauteurRandArrive,},
+		vitesseDuck, function() {
+			checkFinJeu();
 	});
 }
 
-/* Initialisation de l'application */
-function init(){
-	// Change le curseur
-	$('html').css('cursor','crosshair');
+/* Animation de déplacement de droite a gauche*/
+function deplacerDroiteGauche(){
+	//Initialise les propriétés de départ de l'image du canard
+	duck.css({
+		'width': '100px',
+		'height': '70px',
+		'background-image': 'url("media/img/duck-left.gif")',
+		'background-position': '-15px -10px',
+		'left': largeurFenetre,
+		'top': hauteurRandDepart,
+	});
 
-	// Surveille la div du canard (pour le clique pour le tué)
-	duck.on("mousedown", tuerCanard);
+	// Déplace le canard
+	duck.animate({
+		left: '-'+duck.width()+'px',
+		top: hauteurRandArrive,},
+		vitesseDuck, function() {
+			checkFinJeu();
+	});
+}
+
+
+/* Déplacement du canard */
+function tourDeJeu(){
+	nbDuckRestant--;// Décrémente le nombre de canard restant
+
+	// Hauteur aléatoire
+	hauteurRandDepart = Math.random() * (hauteurFenetre - duck.height() - $("#menu").height());
+	hauteurRandArrive = Math.random() * (hauteurFenetre - duck.height() - $("#menu").height());
 	
-	//Lance le canard
-	deplacerCanard();
+	vitesseDuck -= pasDeLaVitesse;// Décremente la vitesse ( = accélération du canard)
+
+	if(Math.random() <= 0.5){
+		deplacerGaucheDroite();
+	} else {
+		deplacerDroiteGauche();
+	}
+}
+
+/* Initialisation de l'application */
+	function init(){
+	//TODO Proposé de changé le nombre de canard
+	nbDuck = 10;// Nombre de Canard pour la partie
+	nbDuckRestant = nbDuck; // Nombre de canard restant lors de la partie
+	largeurFenetre = $(window).width();// Largeur de la fenètre
+	hauteurFenetre = $(window).height();// Hauteur de la fenètre
+	vitesseDuck = 2000;// Vitesse de déplacement du canard
+	pasDeLaVitesse = (2000-1000)/nbDuck;// nombre de ms a ajouté la vitesse a chaque tour : (vitesse lente - vitesse rapide) / nombre de tour
+	// TODO
+	// Créer une écoute sur btn start
+	// Recuperer valeur de l'input
+	playerName = "pierre";//nom du joueur
+	// playerName = prompt("Nom du joueur ?");//nom du joueur
+	score = 0;//score humain
+	$("#score").html("Score : " + score);
+
+	$('html').css('cursor','crosshair');// Change le curseur pour une croix
+	
+	
+	tourDeJeu();// A chaque tour du jeu un canard est lancé
 }
 
 /* MAIN - Initialisation du jeu */
-init();
+duck.on("mousedown", tuerCanard);// Surveille la div du canard (pour le clique pour le tuer)
+$("#nouvelle_partie").on('click', init);
+
 
