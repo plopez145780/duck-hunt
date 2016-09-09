ab1eb73538eda12725034abe4da839037f3369ab
plopez <pierre_albert_lopez@hotmail.fr>
2016-09-10 00:02:33 +0200

    Ajout audio, page start et game over
    Ajout du bruit de fusil
    Ajout page de démarrage avec nom (qui est vérifié)
    Ajout page game over
    Modification div content en fenetre

--- media/js/app.js
+++ media/js/app.js
@@ -1,13 +1,17 @@
 /* Varialbe Globales */
 duck = $("#duck");// référence a l'objet duck
 
+
+
+
+
+
 /* Détermine si tous les canard (tour) on été joué */
 function checkFinJeu() {
 	if(nbDuckRestant != 0){
 		tourDeJeu();
 	} else {
-		//TODO Changer par un texte dans la fenètre
-		alert("Game Over ! Score de " + playerName + " : " + score);
+		pageFin();
 	}
 }
 
@@ -15,6 +19,9 @@ function checkFinJeu() {
 function tuerCanard() {
 	score++;// Incrémente le score
 	$("#score").html("Score : " + score);// Affiche le nouveau score
+	$("audio").get(0).play();
+
+
 	duck.stop(true, false);// Arrète le déplacement horizontal du canard
 	
 	// Change les propriété de l'image
@@ -85,7 +92,8 @@ function tourDeJeu(){
 	// Hauteur aléatoire
 	hauteurRandDepart = Math.random() * (hauteurFenetre - duck.height() - $("#menu").height());
 	hauteurRandArrive = Math.random() * (hauteurFenetre - duck.height() - $("#menu").height());
-	
+	// hauteurRandDepart = Math.random() * (hauteurFenetre - duck.height());
+	// hauteurRandArrive = Math.random() * (hauteurFenetre - duck.height());
 	vitesseDuck -= pasDeLaVitesse;// Décremente la vitesse ( = accélération du canard)
 
 	if(Math.random() <= 0.5){
@@ -95,10 +103,82 @@ function tourDeJeu(){
 	}
 }
 
+function affichageFenetre($box){
+	// met la box avec son message dans la fenetre noir
+	$("#fenetre").append($box);
+	// CSS
+	$("#fenetre").css({
+		'background-color': '#000000',
+		'display' : 'flex',
+		'height': $(window).height(),
+	});
+}
+
+
+function pageDemarrage() {
+
+	$txtPresentation = $("<h1>").append("Bienvenue sur <br>Duck Hunt.");
+
+	$InputPlayerName = $("<input>");
+	$InputPlayerName.attr('type', 'text');
+	$InputPlayerName.attr('placeholder', 'Nom du joueur');
+	$InputPlayerName.attr('id', 'player_name');
+
+	$btnDemarrer = $("<button>");
+	$btnDemarrer.attr('id', 'nouvelle_partie');
+	$btnDemarrer.html("Démarrer");
+	$btnDemarrer.on('click', verifieNom);
+
+	$playerForm = $("<div>");
+	$playerForm.attr('id', 'player_form');
+	$playerForm.append($txtPresentation);
+	$playerForm.append($InputPlayerName);
+	$playerForm.append("<br>");
+	$playerForm.append("<br>");
+	$playerForm.append($btnDemarrer);
+
+	var $box = $("#box");
+	$box.empty();
+	$box.append($txtPresentation);
+	$box.append($playerForm);
+	affichageFenetre($box);
+}
+
+function pageFin() {
+	var $go = $("<h1>").append('Game Over !');
+	var $textScore = $("<p>").append('Score de ').append(playerName).append(' : ').append(score);
+	var $btnNouvellePartie = $("<button>").html("Nouvelle Partie");
+	$btnNouvellePartie.on('click',pageDemarrage);
+	
+	$blocDiv = $("<div>").append($go).append($("<br>")).append($textScore).append("<br>").append($btnNouvellePartie);
+	$box = $("#box").empty().append($blocDiv);
+
+	affichageFenetre($box);//Affiche la fenetre
+}
+
+
+function verifieNom() {
+	if($("#erreur").length <= 0){
+		$("#box>div").append($("<p>"));
+		console.log('creer');
+	}
+	if(!$("#player_name").val()){
+		$("#box>div>p").empty().attr('id', 'erreur').html("Erreur : Le nom n'est pas renseigné");
+	} else {
+		init();
+	}
+}
+
+
 /* Initialisation de l'application */
-	function init(){
+function init(){
+	console.log('init');
+	$("#fenetre").css({
+		'display': 'none',
+	});
+
 	//TODO Proposé de changé le nombre de canard
-	nbDuck = 10;// Nombre de Canard pour la partie
+	nbDuck = 3;// Nombre de Canard pour la partie
 	nbDuckRestant = nbDuck; // Nombre de canard restant lors de la partie
 	largeurFenetre = $(window).width();// Largeur de la fenètre
 	hauteurFenetre = $(window).height();// Hauteur de la fenètre
@@ -107,7 +187,8 @@ function tourDeJeu(){
 	// TODO
 	// Créer une écoute sur btn start
 	// Recuperer valeur de l'input
-	playerName = "pierre";//nom du joueur
+	playerName = $("#player_name").val();
+	// playerName = "pierre";//nom du joueur
 	// playerName = prompt("Nom du joueur ?");//nom du joueur
 	score = 0;//score humain
 	$("#score").html("Score : " + score);
@@ -119,7 +200,8 @@ function tourDeJeu(){
 }
 
 /* MAIN - Initialisation du jeu */
+pageDemarrage();
 duck.on("mousedown", tuerCanard);// Surveille la div du canard (pour le clique pour le tuer)
-$("#nouvelle_partie").on('click', init);
+$("#reset").on('click', pageDemarrage);
 
 
